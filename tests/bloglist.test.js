ce9bd27e12a7ae96b3184216c2719e74b9821efa
NastiaShulgina <75643824+NastiaShulgina@users.noreply.github.com>
2023-05-18 14:03:14 +0300

    4.9: Blog list tests, step2

--- tests/bloglist.test.js
+++ tests/bloglist.test.js
@@ -38,6 +38,15 @@ test('blogs are returned in a correct amount and in the JSON format', async () =
   expect(response.body).toHaveLength(initialBlogs.length)
 })
 
+test('the unique identifier property of the blog posts is named id', async () => {
+  const response = await api.get('/api/blogs')
+
+  response.body.forEach((blog) => {
+    expect(blog.id).toBeDefined()
+    expect(blog._id).toBeUndefined()
+  })
+})
+
 // test('dummy returns one', () => {
 //   const blogs = []
 
